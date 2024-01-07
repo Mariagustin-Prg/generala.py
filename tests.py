ca90cddc11eb608413f58ebdf902525b1498d80f
Mariagustin-Prg_1 <mariagustin.acosta1703@gmail.com>
2024-01-07 13:25:04 -0300

    fix creación de jugadores

--- tests.py
+++ tests.py
@@ -8,17 +8,17 @@ while True:
     n_jugadores += 1
     exec(f"jugador_{n_jugadores} = Jugador(nombre = input(f'Ingrese el nombre del jugador n°{n_jugadores}:'))")
 
-    time.sleep(3)
+    time.sleep(1)
 
-    exec(f'print(f"¿El nombre del jugador n°1 es: {globals()["jugador_" + str(n_jugadores)].nombre}?")')
+    exec(f'print(f"¿El nombre del jugador n°{n_jugadores} es: {globals()["jugador_" + str(n_jugadores)].nombre}?")')
 
     respuesta_jugadores = input("Sí(Y) o No(N): ")
 
     bool_de_respuesta = [True if respuesta_jugadores == 'Y' else False]
 
-    if bool_de_respuesta is False:
+    if bool_de_respuesta[0] is False:
         n_jugadores -= 1
-        break
+        continue
     else:
         pass
 
@@ -26,12 +26,14 @@ while True:
 
     bool_mas_jugadores = [True if mas_jugadores == 'Y' else False]
 
-    if bool_mas_jugadores is True:
-        continue
-    else:
+    if bool_mas_jugadores[0] is False:
         break
+    else:
+        # print(bool_mas_jugadores)
+        time.sleep(1)
+        continue
 
 os.system('cls')
 print("Lista de Jugadores:\n")
 for x in range(1, n_jugadores + 1):
-    exec(f'print(f"Jugador N°1: {globals()["jugador_" + str(x)].nombre}")')
+    exec(f'print(f"Jugador N°{x}: {globals()["jugador_" + str(x)].nombre}")')
