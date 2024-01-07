63177b8cccf1e7851c36fc32e027cbcf7dfefb8d
Mariagustin-Prg_1 <mariagustin.acosta1703@gmail.com>
2024-01-07 14:09:59 -0300

    crea clase Juego_Generala

--- tests.py
+++ tests.py
@@ -1,39 +1,50 @@
 from dados import Jugador, Generala
 import time, os
+import pandas as pd
+
+class Juego_Generala:
+    def __init__(self):
+        self.__generala__ = Generala()
 
 
-n_jugadores = 0
-while True:
-    os.system('cls')
-    n_jugadores += 1
-    exec(f"jugador_{n_jugadores} = Jugador(nombre = input(f'Ingrese el nombre del jugador n°{n_jugadores}:'))")
+    def crear_jugadores(self):
+        n_jugadores = 0
+        while True:
+            os.system('cls')
+            n_jugadores += 1
+            exec(f"jugador_{n_jugadores} = Jugador(nombre = input(f'Ingrese el nombre del jugador n°{n_jugadores}:'))")
 
-    time.sleep(1)
+            time.sleep(1)
 
-    exec(f'print(f"¿El nombre del jugador n°{n_jugadores} es: {globals()["jugador_" + str(n_jugadores)].nombre}?")')
+            exec(f'print(f"¿El nombre del jugador n°{n_jugadores} es: {globals()["jugador_" + str(n_jugadores)].nombre}?")')
 
-    respuesta_jugadores = input("Sí(Y) o No(N): ")
+            respuesta_jugadores = input("Sí(Y) o No(N): ")
 
-    bool_de_respuesta = [True if respuesta_jugadores == 'Y' else False]
+            bool_de_respuesta = [True if respuesta_jugadores == 'Y' else False]
 
-    if bool_de_respuesta[0] is False:
-        n_jugadores -= 1
-        continue
-    else:
+            if bool_de_respuesta[0] is False:
+                n_jugadores -= 1
+                continue
+            else:
+                pass
+
+            mas_jugadores = input("¿Hay más jugadores para agregar? Sí(Y) o No(N). Respuesta:")
+
+            bool_mas_jugadores = [True if mas_jugadores == 'Y' else False]
+
+            if bool_mas_jugadores[0] is False:
+                break
+            else:
+                # print(bool_mas_jugadores)
+                time.sleep(1)
+                continue
+
+        os.system('cls')
+        print("Lista de Jugadores:\n")
+        for x in range(1, n_jugadores + 1):
+            exec(f'print(f"Jugador N°{x}: {globals()["jugador_" + str(x)].nombre}")')
+
+    def planilla(self):
+        # Implementar una estructura de Dataframe, utilizando a los jugadores y 
+        # los valores de sus jugadas como los datos del dataframe.
         pass
-
-    mas_jugadores = input("¿Hay más jugadores para agregar? Sí(Y) o No(N). Respuesta:")
-
-    bool_mas_jugadores = [True if mas_jugadores == 'Y' else False]
-
-    if bool_mas_jugadores[0] is False:
-        break
-    else:
-        # print(bool_mas_jugadores)
-        time.sleep(1)
-        continue
-
-os.system('cls')
-print("Lista de Jugadores:\n")
-for x in range(1, n_jugadores + 1):
-    exec(f'print(f"Jugador N°{x}: {globals()["jugador_" + str(x)].nombre}")')
